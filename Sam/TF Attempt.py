e6e6b24a77fcb0a80b0de1ce8274af1d93986cee
Corpsecreate <samsgwelling@gmail.com>
2018-05-23 01:28:54 +1000

    Update TF Attempt.py

--- Sam/TF Attempt.py
+++ Sam/TF Attempt.py
@@ -34,7 +34,7 @@ data = pd.DataFrame(data_points, columns=['x', 'y', 'y2', 'y3'])
 
 data = data.reset_index(drop=True)
 
-ys = ['y','y2']
+ys = ['y','y2','y3']
 
 plt.plot(data.x, data.y, 'ob') # Plot step
 
@@ -42,23 +42,23 @@ X = tf.placeholder(tf.float32, [None, 1])
 X = tf.reshape(X, [-1, 1]) # I don't know why but it doesnt work without this reshape
 
 # Define number of Neurons per layer
-K        = 15 # Layer 1
-L        = 5  # Layer 2
+K        = 100 # Layer 1
+L        = 50  # Layer 2
 N_INPUT  = 1
 N_OUTPUT = len(ys)
 
 # LAYER 1
 # Initialize weights, normal dist.
-W1 = tf.Variable(tf.truncated_normal([N_INPUT, K], stddev = 0.1))
+W1 = tf.Variable(tf.truncated_normal([N_INPUT, K], stddev = 1))
 # Bias terms initialized to zero
 B1 = tf.Variable(tf.zeros([K]))
 
 # LAYER 2
-W2 = tf.Variable(tf.truncated_normal([K, L], stddev = 0.1))
+W2 = tf.Variable(tf.truncated_normal([K, L], stddev = 1))
 B2 = tf.Variable(tf.zeros([L]))
 
 # LAYER 2
-W3 = tf.Variable(tf.truncated_normal([L, N_OUTPUT], stddev = 0.1))
+W3 = tf.Variable(tf.truncated_normal([L, N_OUTPUT], stddev = 1))
 B3 = tf.Variable(tf.zeros([N_OUTPUT]))
 
 # Feed forward. Output of previous is input to next
@@ -69,8 +69,6 @@ Y  = tf.nn.softmax(tf.matmul(Y2, W3) + B3) # Softmax activation
 
 Y_ = tf.placeholder(tf.float32, [None, N_OUTPUT])
 
-init = tf.global_variables_initializer()
-
 # Loss Function
 # : reduce_sum just sums up the vector 
 #loss = tf.reduce_mean((Y - Y_)**2)
@@ -79,9 +77,11 @@ init = tf.global_variables_initializer()
 loss = -tf.reduce_sum( Y * Y_ )
 
 # Learning Rate
-LR = 0.01
-optimizer 		= tf.train.GradientDescentOptimizer(LR)
-train_step        = optimizer.minimize(loss)
+LR = 0.0002
+optimizer   = tf.train.AdamOptimizer(LR)
+train_step  = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
@@ -91,25 +91,28 @@ errs = []
 shp_x = np.reshape(data.x, (-1, N_INPUT))       # Input data
 shp_y = np.reshape(data[ys], (-1, N_OUTPUT))    # Output 'Labels'
 
+# A perfect model would produce this loss
+desired_loss = -sum(data.y[data.y==1])
+
 t_1 = time.time()
 
-for i in range(10000):
+for i in range(100000):
     
-    train_data = {X: shp_x, Y_: shp_y}
+    batch = data.sample(random.randint(5,5000))
+    train_data = {X: np.reshape(batch.x, (-1, N_INPUT)), Y_: np.reshape(batch[ys], (-1, N_OUTPUT))}
+    #train_data = {X: shp_x, Y_: shp_y}
     sess.run(train_step, feed_dict=train_data)
     
     if i % 100 == 0:
+        train_data = {X: shp_x, Y_: shp_y}
         pred, real, error = sess.run([Y, Y_, loss], feed_dict = train_data)
         errs.append(error)
-        print("Iteration: {:<10.0f} Loss = {:<16.6f}".format(i, errs[-1]))
+        print("Iteration: {:<10.0f} Loss = {:<16.6f} Perfect Loss = {:<16.6f}".format(i, errs[-1], desired_loss))
 
 plt.plot(errs)
 
 y1, y2 = sess.run([Y, Y_], feed_dict = train_data)
 plt.plot(y1)
-
-# A rule based design would produce this output
-desired_loss = -sum(data.y[data.y==1])
 
 # if learning properly, the first column of y1 should alternate between 1 and 0,
 # depending purely on the value of x. The loss function should equal to the negative sum
