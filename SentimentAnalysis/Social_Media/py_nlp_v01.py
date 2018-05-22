1e6f0d56e38469fe6a21a94abb0f78e77a799046
rendorHaevyn <34834285+rendorHaevyn@users.noreply.github.com>
2018-05-23 00:47:21 +1000

    update

---
+++ SentimentAnalysis/Social_Media/py_nlp_v01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 21:29:27 2018
+
+@author: rendorHaevyn
+@info-urls:
+> https://www.kaggle.com/c/word2vec-nlp-tutorial#part-1-for-beginners-bag-of-words
+> 
+"""
+
+import nltk
+from nltk.corpus import stopwords
+stopwords.words("english")
