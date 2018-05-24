1e36cf99e8cd7ab4f375dfd187b60f67e151df0e
Corpsecreate <samsgwelling@gmail.com>
2018-05-24 20:43:05 +1000

    Update TF_DummyData.py

--- Sam/TF_DummyData.py
+++ Sam/TF_DummyData.py
@@ -33,9 +33,6 @@ data = pd.DataFrame(data_points, columns=['x', 'y1', 'y2', 'y3'])
 data = data.sort_values('x')
 data = data.reset_index(drop=True)
 
-shp_x = np.reshape(data.x, (-1, N_INPUT))       # Input data
-shp_y = np.reshape(data[ys], (-1, N_OUTPUT))    # Output 'Labels
-
 ys = ['y1','y2', 'y3']
 
 plt.plot(data.x, data.y1, 'ob') # Plot step
@@ -50,6 +47,9 @@ L        = 10  # Layer 2
 N_INPUT  = 1
 N_OUTPUT = len(ys)
 
+shp_x = np.reshape(data.x, (-1, N_INPUT))       # Input data
+shp_y = np.reshape(data[ys], (-1, N_OUTPUT))    # Output 'Labels
+
 # LAYER 1
 # Initialize weights, normal dist.
 W1 = tf.Variable(tf.truncated_normal([N_INPUT, K], stddev = 0.1))
@@ -60,15 +60,16 @@ B1 = tf.Variable(tf.zeros([K]))
 W2 = tf.Variable(tf.truncated_normal([K, L], stddev = 0.1))
 B2 = tf.Variable(tf.zeros([L]))
 
-# LAYER 2
+# LAYER 3
 W3 = tf.Variable(tf.truncated_normal([L, N_OUTPUT], stddev = 0.1))
 B3 = tf.Variable(tf.zeros([N_OUTPUT]))
 
 # Feed forward. Output of previous is input to next
 # Activation function for final layer is Softmax for probabilties in the range [0,1]
-Y1 = tf.nn.relu(tf.matmul(X,  W1) + B1)
-Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
-Y  = tf.nn.softmax(tf.matmul(Y2, W3) + B3) # Softmax activation
+L1 = tf.nn.relu(tf.matmul(X,  W1) + B1)
+L2 = tf.nn.relu(tf.matmul(L1, W2) + B2)
+
+Y  = tf.nn.softmax(tf.matmul(L2, W3) + B3) # Softmax activation
 
 Y_ = tf.placeholder(tf.float32, [None, N_OUTPUT])
 
@@ -80,8 +81,8 @@ Y_ = tf.placeholder(tf.float32, [None, N_OUTPUT])
 neg_reward = -tf.reduce_sum( Y * Y_ )
 
 # Learning Rate
-LR = 0.0001
-optimizer 		= tf.train.AdamOptimizer(LR)
+LR = 0.01
+optimizer 		= tf.train.GradientDescentOptimizer(LR)
 train_step        = optimizer.minimize(neg_reward)
 
 init = tf.global_variables_initializer()
@@ -105,8 +106,8 @@ plt.show()
 perfect_reward = -sum(data.y1[data.y1==1])
 
 use_mini_batch   = True
-min_batch_sz     = 100
-max_batch_sz     = 10000
+min_batch_sz     = 1
+max_batch_sz     = 5
 training_rewards = []
 N_ITERATIONS     = 20000
 
@@ -114,13 +115,17 @@ N_ITERATIONS     = 20000
 for i in range(N_ITERATIONS):
     
     if use_mini_batch and i % 100 != 0:
-        sample = data.sample(random.randint(min_batch_sz, max_batch_sz))
+        smpl_size = random.randint(min_batch_sz, max_batch_sz)
+        smpl_size *= 2
+        zeros = data[data.y1 == -1].sample(smpl_size//2)
+        ones = data[data.y1 == 1].sample(smpl_size//2)
+        sample = zeros.append(ones)
+        #sample = data.sample()
         train_data = {X: np.reshape(sample.x, (-1, N_INPUT)), \
                       Y_: np.reshape(sample[ys], (-1, N_OUTPUT))}
+        sess.run(train_step, feed_dict=train_data)
     else:
         train_data = {X: shp_x, Y_: shp_y}
-
-    sess.run(train_step, feed_dict=train_data)
     
     if i % 100 == 0:
         pred, real, reward = sess.run([Y, Y_, neg_reward], feed_dict = train_data)
@@ -130,7 +135,7 @@ for i in range(N_ITERATIONS):
         
 #---------------------------------------------------------------------------
 
-plt.plot(training_rewards)
+plt.plot(training_rewards[100:])
 plt.show()
 
 train_data = {X: shp_x, Y_: shp_y}
