d3d80f3143a37467a83dda8c59e2cc2eb0c396cd
Corpsecreate <samsgwelling@gmail.com>
2018-05-25 08:57:56 +1000

    Update CryptoAI.py

--- Sam/CryptoAI.py
+++ Sam/CryptoAI.py
@@ -11,19 +11,20 @@ import re
 
 random.seed(1)
 
-data = pd.read_csv("M15/ALL.csv").dropna(axis=0, how='any').reset_index(drop=True)
+print("Loading Data...", end="")
+data = pd.read_csv("M5/ALL.csv").dropna(axis=0, how='any').reset_index(drop=True)
 data.drop('date', axis=1, inplace=True)
-data = data[int(len(data)*0.3):len(data)].reset_index(drop=True)
+print("{} rows & {} columns".format(len(data), len(data.columns)))
+data = data[int(len(data)*0.2):len(data)].reset_index(drop=True)
 
 INCLUDE_VOLUME = True
 ALLOW_SHORTS   = False
 DISCOUNT       = True
-DISCOUNT_STEPS = 4
-GAMMA          = 0.6
+DISCOUNT_STEPS = 24
+GAMMA          = 0.95
 
-ASSETS      = ['USD', 'BTC', 'ETH', 'BCH', 'XRP', 'DASH', 'XMR', 'ZEC', 'LTC', 'LTC']
+ASSETS      = ['USD', 'BTC', 'ETH', 'BCH', 'XRP', 'DASH', 'XMR', 'ZEC', 'LTC']
 INPUT_ASSET = []#['BTC', 'BCH', 'ETH']
-N_LAGS      = 10
 N_VEC       = 3 + 1 if INCLUDE_VOLUME else 0
 N_ASSETS    = ( len(ASSETS) * 2 - 1 ) if ALLOW_SHORTS else len(ASSETS)
 
@@ -35,7 +36,7 @@ for c in data.columns:
     if not ASSETS:
         cols.append(c)
     else:
-        for a in ASSETS:
+        for a in set(ASSETS):
             if a in c:
                 cols.append(c)
                 break
@@ -53,7 +54,7 @@ for c in data.columns:
     if not INPUT_ASSET:
         cols2.append(c)
     else:
-        for a in INPUT_ASSET:
+        for a in set(INPUT_ASSET):
             if a in c:
                 cols2.append(c)
                 break
@@ -68,13 +69,16 @@ else:
     COLS_X = [x for x in cols2 if ('L_' in x and "VOLUME" not in x) or "M_" in x]
 COLS_Y = ["reward_USD"] + [y for y in cols if 'reward' in y and "USD" not in y]
 
-data_imm = data.copy()
-
 if DISCOUNT:
+    data_imm = data.copy()
     stmt = "data[COLS_Y] = data[COLS_Y]"
     for ahead in range(1,DISCOUNT_STEPS+1):
         stmt += "+(GAMMA**{}) * data[COLS_Y].shift({})".format(ahead, -ahead)
+    print("Calculating Discount Rewards...", end="")
     exec(stmt)
+    print("Done")
+else:
+    data_imm = data
 
 data = data.dropna(axis=0, how='any').reset_index(drop=True)
 
@@ -90,11 +94,13 @@ N_IN  = len(COLS_X)
 N_OUT = len(COLS_Y)
 
 # Define number of Neurons per layer
-K = 80 # Layer 1
-L = 50 # Layer 2
-M = 25  # Layer 3
+K = 700 # Layer 1
+L = 600  # Layer 2
+M = 500  # Layer 3
+N = 400  # Layer 4
+O = 300  # Layer 5
 
-SDEV = 0.01
+SDEV = 0.1
 
 # LAYER 1
 # Initialize weights, normal dist.
@@ -111,34 +117,43 @@ W3 = tf.Variable(tf.random_normal([L, M], stddev = SDEV))
 B3 = tf.Variable(tf.random_normal([M]))
 
 # LAYER 4
-W4 = tf.Variable(tf.random_normal([M, N_OUT], stddev = SDEV))
-B4 = tf.Variable(tf.random_normal([N_OUT]))
+W4 = tf.Variable(tf.random_normal([M, N], stddev = SDEV))
+B4 = tf.Variable(tf.random_normal([N]))
+
+# LAYER 5
+W5 = tf.Variable(tf.random_normal([N, N_OUT], stddev = SDEV))
+B5 = tf.Variable(tf.random_normal([N_OUT]))
+
+# LAYER 6
+#W6 = tf.Variable(tf.random_normal([O, N_OUT], stddev = SDEV))
+#B6 = tf.Variable(tf.random_normal([N_OUT]))
 
 X = tf.placeholder(tf.float32, [None, N_IN])
 X = tf.reshape(X, [-1, N_IN])
 
 # Feed forward. Output of previous is input to next
 # Activation function for final layer is Softmax for probabilties in the range [0,1]
-Y1 = tf.nn.relu(tf.matmul(X,  W1) + B1)
-Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
-Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
-Y =  tf.nn.softmax(tf.matmul(Y3, W4) + B4)
+Y1 = tf.nn.leaky_relu(tf.matmul(X,  W1) + B1)
+Y2 = tf.nn.leaky_relu(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.leaky_relu(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.leaky_relu(tf.matmul(Y3, W4) + B4)
+#Y5 = tf.nn.leaky_relu(tf.matmul(Y4, W5) + B5)
+Y =  tf.nn.softmax(tf.matmul(Y4, W5) + B5)
 #Y  = (tf.matmul(Y4, W5) + B5)
 
 # Placeholder for correct answers, digit recognition so theres 10 output probabilities
 Y_ = tf.placeholder(tf.float32, [None, N_OUT])
 
-loss = -tf.reduce_mean( 10000 * (Y * Y_) )
+loss = -tf.reduce_mean( 10000000 * (Y * Y_) )
 
 # Optimizer
-LEARNING_RATE 	= 0.00002
+LEARNING_RATE 	= 0.00001
 optimizer 		= tf.train.AdamOptimizer(LEARNING_RATE)
 train_step 		= optimizer.minimize(loss)
 
-
-BATCH_SZ_MIN = 100#round(0.05*len(data))
-BATCH_SZ_MAX = 400#round(0.2*len(data))
-TEST_LEN     = round(0.15*len(data))
+BATCH_SZ_MIN = 4#round(0.05*len(data))
+BATCH_SZ_MAX = 24#round(0.2*len(data))
+TEST_LEN     = round(0.2*len(data))
 IDX_MAX      = len(data) - TEST_LEN - BATCH_SZ_MAX - 1
 
 test_imm   = data_imm.iloc[len(data_imm)-TEST_LEN:, :].reset_index(drop=True)
@@ -159,7 +174,7 @@ while True:
     
     sess.run(init)
     init_rewards.append(-sess.run(loss,feed_dict=feed_imm))
-    if len(init_rewards) > 100:
+    if len(init_rewards) > 50:
         if init_rewards[-1] >= pd.Series(init_rewards).describe()[6]:
             break
         
@@ -185,8 +200,10 @@ for i in range(1000000):
         idx      = round(random.random()**0.5*IDX_MAX)
         batch_sz = random.randint(BATCH_SZ_MIN, BATCH_SZ_MAX)
         sub_data = data.iloc[idx:idx+batch_sz, :].reset_index(drop=True)
+        sub_data = data.iloc[:IDX_MAX,:].sample(batch_sz)
         batch_X, batch_Y = (sub_data[COLS_X], sub_data[COLS_Y])
-        train_data = {X: np.reshape(batch_X, (-1,N_IN)), Y_: np.reshape(batch_Y, (-1, N_OUT))}
+        train_data = {X:  np.reshape(batch_X, (-1, N_IN)), 
+                      Y_: np.reshape(batch_Y, (-1, N_OUT))}
         sess.run(train_step, feed_dict=train_data)
         
     #batch_X = np.matrix(batch_X)
@@ -205,6 +222,7 @@ for i in range(1000000):
 
 plt.plot(dat_rwds)
 plt.plot(imm_rwds)
+plt.legend(['Discount Test Reward', 'Actual Test Reward'])
 plt.show()
 
 y1, y2 = sess.run([Y, Y_], feed_dict = feed_imm)
@@ -215,6 +233,7 @@ for x in y3:
     prof.append(prof[-1]+sum(x))
     
 plt.plot(prof)
+plt.legend(['Actual Reward'])
 plt.show()
 
 long_short = [[] for _ in range(3 if ALLOW_SHORTS else 2)]
@@ -229,7 +248,7 @@ for val in y1:
     else:
         long_short[1].append(sum(val[1:]))
 
-rolling_window = 25
+rolling_window = 100
 for i in range(len(long_short)):
     dat = pd.rolling_mean(pd.Series(long_short[i]),rolling_window)
     plt.plot(dat)
@@ -239,5 +258,6 @@ plt.show()
 for i in range(len(props)):
     dat = pd.rolling_mean(pd.Series(props[i]),rolling_window)
     plt.plot(dat)
-#plt.legend([x[x.index("_")+1:] for x in COLS_Y])
+plt.legend([x[x.index("_")+1:] for x in COLS_Y])
 plt.show()
+plt.ion()
