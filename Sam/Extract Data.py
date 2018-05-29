6f7ee380c82ce326b786953f6b4da623bcfb5667
Corpsecreate <samsgwelling@gmail.com>
2018-05-29 20:30:51 +1000

    Update Extract Data.py

--- Sam/Extract Data.py
+++ Sam/Extract Data.py
@@ -25,19 +25,21 @@ def ExtractData(N_LAGS=5):
 
     JUNE_1_2017 = 1496275200
 
-
     tickers = []
     for T in ALL_TICKERS:
         tickers.append((T, T[:T.index("_")], T[T.index("_")+1:], 1 if "USDT" in T else 0))
 
-    #coin_list = pd.DataFrame(tickers)
-    #coin_list.columns = ['Pair', 'Base','Coin', 'Extract']
-    #coin_list.to_csv("coins.csv", index=False)
+    coin_list = pd.DataFrame(tickers)
+    coin_list.columns = ['Pair', 'Base','Coin', 'Extract']
+    coin_list.to_csv("coins.csv", index=False)
 
     tm = time.time()
 
     tmap    = {86400 : "D1", 14400 : "H4", 7200 : "H2", 1800 : "M30", 900 : "M15", 300 : "M5"}
     highmap = {86400 : 900,  14400 : 300,  7200 : 300,  1800 : 300,   900 : 300,   300 : 300 }
+
+    SHIFT_POINTS_HOURS = [24 * 30, 24 * 7, 24 * 3, 24, 0]
+    REG_HOURS = [24 * 30, 24 * 7, 24 * 3, 24, 4, 2]
 
     CALC_R2 = False
 
@@ -79,10 +81,93 @@ def ExtractData(N_LAGS=5):
                     if col_type == 'L_VOLUME':
                         col = 'volume'
                     df["{}_{}".format(col_type, lag+1)] = df[col].shift((lag+1)) / df['close' if col != 'volume' else 'volume'].shift(0)
+            
+            #-------------------------------------------------------------------
+            for i in range(len(SHIFT_POINTS_HOURS)-1):
+                look_back1 = (SHIFT_POINTS_HOURS[i+0] * 3600) // tf
+                look_back2 = (SHIFT_POINTS_HOURS[i+1] * 3600) // tf
+                df["L_CHNG_{}".format(i+1)] = df['close'].shift(look_back2) / df['close'].shift(look_back1)
+            #-------------------------------------------------------------------
+            #-------------------------------------------------------------------
+            for i in range(len(SHIFT_POINTS_HOURS)-1):
+                L = len(df)
+                look_back1 = (SHIFT_POINTS_HOURS[i+0] * 3600) // tf
+                look_back2 = (SHIFT_POINTS_HOURS[i+1] * 3600) // tf
+                coefs = []
+                for row_n in range(L):
+                    try:
+                        idx1 = row_n - look_back1
+                        if idx1 < 0:
+                            coefs.append(np.nan)
+                            continue
+                        idx2 = row_n - look_back2
+                        data = df.ix[idx1:idx2,'close'] / df.ix[idx1,'close']
+                        data = data.apply(lambda x : math.log10(x))
+                        coeff = np.linalg.lstsq(np.reshape(range(len(data)), (-1, 1)), data)[0]
+                        #print("Coefficients", coeff)
+                        coefs.append(coeff[0])
+                        #print(coefs)
+                    except Exception as err:
+                        print("error is ", err, idx1, idx2, df.ix[idx1,'close'])
+                        coefs.append(np.nan)
+                df["L_REG_CLOSE_{}".format(i+1)] = coefs
+
+            for i, hours in enumerate(REG_HOURS):
+                L = len(df)
+                look_back1 = (hours * 3600) // tf
+                coefs = []
+                for row_n in range(L):
+                    try:
+                        idx1 = row_n - look_back1
+                        if idx1 < 0:
+                            coefs.append(np.nan)
+                            continue
+                        data = df.ix[idx1:(row_n+1),'close'] / df.ix[idx1,'close']
+                        data = data.apply(lambda x : math.log10(x))
+                        coeff = np.linalg.lstsq(np.reshape(range(len(data)), (-1, 1)), data)[0]
+                        coefs.append(coeff[0])
+                    except Exception as err:
+                        print("error is ", err, idx1)
+                        coefs.append(np.nan)
+                df["REG_CLOSE_{}".format(i+1)] = coefs
+
+            for i, hours in enumerate(REG_HOURS):
+                L = len(df)
+                look_back1 = (hours * 3600) // tf
+                coefs = []
+                for row_n in range(L):
+                    try:
+                        idx1 = row_n - look_back1
+                        if idx1 < 0:
+                            coefs.append(np.nan)
+                            continue
+                        denom = df.ix[idx1,'volume']
+                        loops = 1
+                        broke = False
+                        while denom == 0:
+                            new_idx = idx1 - loops
+                            if new_idx < 0:
+                                coefs.append(0)
+                                broke = True
+                                break
+                            denom = df.ix[new_idx,'volume']
+                            loops += 1
+                        if broke:
+                            continue
+                        data = df.ix[idx1:(row_n+1),'volume'] / denom
+                        #data = data.apply(lambda x : math.log10(x))
+                        coeff = np.linalg.lstsq(np.reshape(range(len(data)), (-1, 1)), data)[0]
+                        coefs.append(coeff[0])
+                    except Exception as err:
+                        print("error is ", err, denom)
+                        coefs.append(np.nan)
+                df["REG_VOLUME_{}".format(i+1)] = coefs
+                
+            #-------------------------------------------------------------------
             df=df[['date']+[c for c in df.columns if c != 'date']]
             newnames = [c if c == "date" else c+TICK[4:] for c in df.columns]
             df.columns = newnames
-            
+            #-------------------------------------------------------------------
             df.to_csv("{}/{}.csv".format(tmap[tf], TICK), index=False)
             print("Wrote: {} {}".format(TICK, tmap[tf]))
             if merged is None:
@@ -91,4 +176,4 @@ def ExtractData(N_LAGS=5):
                 merged = merged.merge(df, how='inner', on='date')
         merged.to_csv("{}/{}.csv".format(tmap[tf], "ALL"), index=False)
 
-ExtractData(10)
+ExtractData(20)
